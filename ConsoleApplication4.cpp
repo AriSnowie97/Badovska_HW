255b2f96552fb07f4a7ad051c11adb7e52828ae0
Ari Badovska <160342977+AriSnowie97@users.noreply.github.com>
2024-09-21 15:54:37 +0300

    Add files via upload

---
+++ ConsoleApplication4.cpp
@@ -0,0 +1,19 @@
+﻿#include <iostream>
+#include <cmath>
+#define PI 3.14159
+using namespace std;
+
+int main()
+{
+    float Z1, Z2, a;
+    cout << "Enter number a: ";
+    cin >> a;
+
+    Z1 = cos(a) + sin(a) + 3 * cos(a) + 3 * sin(a);
+    Z2 = 2 * sqrt(2) * cos(a) * sin(PI / 4 + 2 * a);
+
+    cout << "Z1: " << Z1 << "\n";
+    cout << "Z2: " << Z2 << endl;
+
+    return 0;
+}
